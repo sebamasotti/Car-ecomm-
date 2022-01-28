d169b288ce9f36b1fa81bc872c3c81acc5a6ae28
sebamasotti <masottisebastian@gmail.com>
2022-01-28 11:01:41 -0300

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="navbar">
+        <h1> ECommerce</h1>
+        <ul>
+          <li>Home</li>
+          <li>Productos</li>
+          <li>Nosotros</li>
+          <li>Contacto</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
